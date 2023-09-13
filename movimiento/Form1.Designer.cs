585fb8196f0e22e445c6330bd3bce6f2c7cae9e7
Usuario <Usuario@DESKTOP-9T6HSJ2>
2023-09-13 08:37:44 -0300

    Agregar archivos de proyecto.

---
+++ movimiento/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace movimiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btInicio = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btInicio
+            // 
+            this.btInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btInicio.Location = new System.Drawing.Point(54, 22);
+            this.btInicio.Name = "btInicio";
+            this.btInicio.Size = new System.Drawing.Size(99, 47);
+            this.btInicio.TabIndex = 1;
+            this.btInicio.Text = "Inicio";
+            this.btInicio.UseVisualStyleBackColor = true;
+            this.btInicio.Click += new System.EventHandler(this.btInicio_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(168, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 50);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btInicio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btInicio;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ movimiento/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movimiento
+{
+    public partial class Form1 : Form
+    {
+        int velocidad = 10;
+        int pasoH = 10;
+        int pasoV = 5;
+        int posicionH = 0;
+        int posicionV = 0;
+        int velocidad2 = 5;
+        int pasoH2 = 5;
+        int pasoV2 = 2;
+        int posicionH2 = 0;
+        int posicionV2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private async void btInicio_Click(object sender, EventArgs e)
+        {
+            btInicio.Visible = false;
+            Label figura2 = new Label();
+            Label figura = new Label();
+            figura.BackColor = Color.Red;
+            figura.Text = "";
+            figura.Top = posicionV;
+            figura.Left = posicionH;
+            figura.Width = 50;
+            figura.Height = 50;
+            Controls.Add(figura);
+
+            figura2.BackColor = Color.Blue;
+            figura.Text = "";
+            figura.Top = posicionV2;
+            figura.Left = posicionH2;
+            figura.Width = 50;
+            figura.Height = 50;
+            Controls.Add(figura2);
+
+            for (int i = 0; i < this.Width; i++)
+            {
+                posicionH2 = posicionH2 + pasoH2;
+                figura2.Left = posicionH2;
+                posicionV2 = posicionV2 + pasoV2;
+                figura2.Top = posicionV2;
+                if ((posicionH2 + figura2.Width) > this.Width)
+                {
+                    pasoH2 = -1 * pasoH2;
+                }
+                else if (posicionH2 < 0)
+                {
+                    pasoH2 = -1 * pasoH2;
+                }
+                if ((posicionV2 + figura2.Height) > this.Height)
+                {
+                    pasoV2 = -1 * pasoV2;
+                }
+                else if (posicionV2 < 0)
+                {
+                    pasoV2 = -1 * pasoV2;
+                }
+
+                this.Refresh();
+                figura2.Top = figura2.Top + pasoV2;
+                figura2.Left = figura2.Left + pasoH2;
+                await Task.Delay(velocidad2);
+
+
+                for (int j = 0; j < this.Width; j++)
+                {
+                    posicionH = posicionH + pasoH;
+                    figura.Left = posicionH;
+                    posicionV = posicionV + pasoV;
+                    figura.Top = posicionV;
+                    if ((posicionH + figura.Width) > this.Width)
+                    {
+                        pasoH = -1 * pasoH;
+                    }
+                    else if (posicionH < 0)
+                    {
+                        pasoH = -1 * pasoH;
+                    }
+                    if ((posicionV + figura.Height) > this.Height)
+                    {
+                        pasoV = -1 * pasoV;
+                    }
+                    else if (posicionV < 0)
+                    {
+                        pasoV = -1 * pasoV;
+                    }
+
+                    this.Refresh();
+                    figura.Top = figura.Top + pasoV;
+                    figura.Left = figura.Left + pasoH;
+                    await Task.Delay(velocidad);
+                }
+
+            }
+        }
+    }
+}
